ee856b7121edd27d6e714ebd659c1d2c36eb6d8c
Upendra672 <upendaryadav672@gmail.com>
2024-04-06 22:05:41 +0530

    chore: Added Material Ui Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import './App.css'
+import { Box, Button, Stack } from "@mui/material";
+import "./App.css";
 
 function App() {
-
   return (
     <>
+    {/* tailwind css example */}
       <div className="text-3xl text-green-400 font-bold underline">
         hello folks...
-       </div>
+      </div>
+
+      {/* material ui example using buttons */}
+      <Box>
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>
+</React.StrictMode>,
 )
